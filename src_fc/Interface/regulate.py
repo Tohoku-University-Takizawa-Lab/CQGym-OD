8a0337911bc6f6ef73267c012da0b145d84c2dc2
Hang <cuihang19910630@163.com>
2024-05-27 15:03:46 -0700

    [feature] regulate.py
    - regulate.py is to change job index to start with 0 so that it can be consistent with CQSim design.

---
+++ src_fc/Interface/regulate.py
@@ -0,0 +1,26 @@
+import sys
+
+
+def main():
+    args = sys.argv
+    fileName = args[1]
+    #newFileName = "".join(fileName.split(".")[:-1]) + "_regulate.swf"
+    fin = open("../data/InputFiles/" + args[1], 'r')
+    fout = open("../data/InputFiles/" + args[2], 'w')
+    lines = fin.readlines()
+    fin.close()
+    ls = ""
+    cnt = 0
+    for line in lines:
+        if line[0] == ';':
+            ls += line
+        else:
+            new_line = ' '.join(line.split())
+            new_line = ' '.join([str(cnt)] + new_line.split(' ')[1:]) + '\n'
+            ls += new_line
+            cnt += 1
+    fout.write(ls)
+    fout.close()
+
+if __name__ == "__main__":
+    main()
